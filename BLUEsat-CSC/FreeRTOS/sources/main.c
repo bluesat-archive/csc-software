989c6782f301e037a08c2c78e09ce64c6216c7fc
jamesq <jamesq@da64b807-da57-4bc4-8482-a38030bc0b9e>
2012-03-09 10:18:57 +0000

    Inserted main.c into code base

---
+++ BLUEsat-CSC/FreeRTOS/sources/main.c
@@ -0,0 +1,186 @@
+/*
+    FreeRTOS V6.1.1 - Copyright (C) 2011 Real Time Engineers Ltd.
+
+    ***************************************************************************
+    *                                                                         *
+    * If you are:                                                             *
+    *                                                                         *
+    *    + New to FreeRTOS,                                                   *
+    *    + Wanting to learn FreeRTOS or multitasking in general quickly       *
+    *    + Looking for basic training,                                        *
+    *    + Wanting to improve your FreeRTOS skills and productivity           *
+    *                                                                         *
+    * then take a look at the FreeRTOS books - available as PDF or paperback  *
+    *                                                                         *
+    *        "Using the FreeRTOS Real Time Kernel - a Practical Guide"        *
+    *                  http://www.FreeRTOS.org/Documentation                  *
+    *                                                                         *
+    * A pdf reference manual is also available.  Both are usually delivered   *
+    * to your inbox within 20 minutes to two hours when purchased between 8am *
+    * and 8pm GMT (although please allow up to 24 hours in case of            *
+    * exceptional circumstances).  Thank you for your support!                *
+    *                                                                         *
+    ***************************************************************************
+
+    This file is part of the FreeRTOS distribution.
+
+    FreeRTOS is free software; you can redistribute it and/or modify it under
+    the terms of the GNU General Public License (version 2) as published by the
+    Free Software Foundation AND MODIFIED BY the FreeRTOS exception.
+    ***NOTE*** The exception to the GPL is included to allow you to distribute
+    a combined work that includes FreeRTOS without being obliged to provide the
+    source code for proprietary components outside of the FreeRTOS kernel.
+    FreeRTOS is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+    more details. You should have received a copy of the GNU General Public
+    License and the FreeRTOS license exception along with FreeRTOS; if not it
+    can be viewed here: http://www.freertos.org/a00114.html and also obtained
+    by writing to Richard Barry, contact details for whom are available on the
+    FreeRTOS WEB site.
+
+    1 tab == 4 spaces!
+
+    http://www.FreeRTOS.org - Documentation, latest information, license and
+    contact details.
+
+    http://www.SafeRTOS.com - A version that is certified for use in safety
+    critical systems.
+
+    http://www.OpenRTOS.com - Commercial support, development, porting,
+    licensing and training services.
+*/
+
+/*
+	NOTE : Tasks run in system mode and the scheduler runs in Supervisor mode.
+	The processor MUST be in supervisor mode when vTaskStartScheduler is
+	called.  The demo applications included in the FreeRTOS.org download switch
+	to supervisor mode prior to main being called.  If you are not using one of
+	these demo application projects then ensure Supervisor mode is used.
+*/
+
+#include "lpc24xx.h"
+
+/* Scheduler includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+
+
+/*-----------------------------------------------------------*/
+
+/* Constants to setup the PLL. */
+#define mainPLL_MUL			( ( unsigned portLONG ) 0x000B )
+#define mainPLL_DIV			( ( unsigned portLONG ) 0x0000 )
+#define mainCPU_CLK_DIV		( ( unsigned portLONG ) 0x0003 )
+#define mainPLL_ENABLE		( ( unsigned portLONG ) 0x0001 )
+#define mainPLL_CONNECT		( ( ( unsigned portLONG ) 0x0002 ) | mainPLL_ENABLE )
+#define mainPLL_FEED_BYTE1	( ( unsigned portLONG ) 0xaa )
+#define mainPLL_FEED_BYTE2	( ( unsigned portLONG ) 0x55 )
+#define mainPLL_LOCK		( ( unsigned portLONG ) 0x4000000 )
+#define mainPLL_CONNECTED	( ( unsigned portLONG ) 0x2000000 )
+#define mainOSC_ENABLE		( ( unsigned portLONG ) 0x20 )
+#define mainOSC_STAT		( ( unsigned portLONG ) 0x40 )
+#define mainOSC_SELECT		( ( unsigned portLONG ) 0x01 )
+
+/* Constants to setup the MAM. */
+#define mainMAM_TIM_3		( ( unsigned portCHAR ) 0x03 )
+//#define mainMAM_MODE_FULL	( ( unsigned portCHAR ) 0x02 )
+#define mainMAM_MODE_FULL	( ( unsigned char ) 0x00 ) // MAM is turned off due to mem restrictions of lpc2109
+
+
+/* Priorities for the demo application tasks. */
+#define HIGH_PRIORITY		( tskIDLE_PRIORITY + 10 )
+#define LOW_PRIORITY		( tskIDLE_PRIORITY + 5  )
+
+
+#define VECT_ADDR_INDEX	0x100
+#define VECT_CNTL_INDEX 0x200
+#define VECT_PRIO_INDEX 0x200
+#define MEMMAP_FLASH  1                 // Interrupt Vectors in Flash
+/*-----------------------------------------------------------*/
+
+/*
+ * Configure the processor for use with the Olimex demo board.  This includes
+ * setup for the I/O, system clock, and access timings.
+ */
+static void prvSetupHardware( void );
+
+int main( void )
+{
+	prvSetupHardware();
+
+	//INSERT TASK KICKER HERE
+
+	/* Now all the tasks have been started - start the scheduler.
+
+	NOTE : Tasks run in system mode and the scheduler runs in Supervisor mode.
+	The processor MUST be in supervisor mode when vTaskStartScheduler is
+	called.  The demo applications included in the FreeRTOS.org download switch
+	to supervisor mode prior to main being called.  If you are not using one of
+	these demo application projects then ensure Supervisor mode is used here. */
+	vTaskStartScheduler();
+
+	return 0;
+}
+
+
+/*-----------------------------------------------------------*/
+
+static void prvSetupHardware( void )
+{
+	#ifdef RUN_FROM_RAM
+		/* Remap the interrupt vectors to RAM if we are are running from RAM. */
+		SCB_MEMMAP = 2;
+	#endif
+
+	// Disable the PLL.
+	PLLCON = 0;
+	PLLFEED = mainPLL_FEED_BYTE1;
+	PLLFEED = mainPLL_FEED_BYTE2;
+
+	// Configure clock source.
+	SCS |= mainOSC_ENABLE;
+	while( !( SCS & mainOSC_STAT ) );
+	CLKSRCSEL = mainOSC_SELECT;
+
+	// Setup the PLL to multiply the XTAL input by 4.
+	PLLCFG = mainPLL_MUL;
+	PLLFEED = mainPLL_FEED_BYTE1;
+	PLLFEED = mainPLL_FEED_BYTE2;
+
+	// Turn on and wait for the PLL to lock...
+	PLLCON = mainPLL_ENABLE;
+	PLLFEED = mainPLL_FEED_BYTE1;
+	PLLFEED = mainPLL_FEED_BYTE2;
+	CCLKCFG = mainCPU_CLK_DIV;
+
+	//USBCLKCFG = mainCPU_CLK_DIV;
+
+
+	while( !( PLLSTAT & mainPLL_LOCK ) );
+
+	// Connecting the clock.
+	PLLCON = mainPLL_CONNECT;
+	PLLFEED = mainPLL_FEED_BYTE1;
+	PLLFEED = mainPLL_FEED_BYTE2;
+	while( !( PLLSTAT & mainPLL_CONNECTED ) );
+
+	/*
+	This code is commented out as the MAM does not work on the original revision
+	LPC2368 chips.  If using Rev B chips then you can increase the speed though
+	the use of the MAM.
+
+	Setup and turn on the MAM.  Three cycle access is used due to the fast
+	PLL used.  It is possible faster overall performance could be obtained by
+	tuning the MAM and PLL settings.
+	MAMCR = 0;
+	MAMTIM = mainMAM_TIM_3;
+	MAMCR = mainMAM_MODE_FULL;
+	*/
+
+	// set the interrupt controller defaults
+	MEMMAP = MEMMAP_FLASH;                // map interrupt vectors space into FLASH
+}
+
+
+
