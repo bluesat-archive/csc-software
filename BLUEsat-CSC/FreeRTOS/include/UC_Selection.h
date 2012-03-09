1d90fe927bccb952783e1aa4d4ddfb918245e3e1
jamesq <jamesq@da64b807-da57-4bc4-8482-a38030bc0b9e>
2012-03-09 10:42:52 +0000

    #Structure renaming sources -> src added test in component folder
    #added freertos header files

---
+++ BLUEsat-CSC/FreeRTOS/include/UC_Selection.h
@@ -0,0 +1,19 @@
+/**
+ *  \file UC_Selection.h
+ *
+ *  \brief This include serves the purpose of centralising the
+ *  chip address definitions for the vaious UCs in use. Changing
+ *  the Chip definition here changes it through out the CSC.
+ *
+ *  \author $Author: Colin Tan $
+ *  \version 1.0
+ *
+ *  $Date: 2010-10-09 09:23:13 +1000 (Sat, 09 Oct 2010) $
+ *
+ */
+#ifndef UC_SELECTION_H_
+#define UC_SELECTION_H_
+
+#include "lpc24xx.h"
+
+#endif /* UC_SELECTION_H_ */
