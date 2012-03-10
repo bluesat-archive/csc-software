ddd4f6e502b85e9fd204fb795763e353ba4fd2fe
ctan <ctan@da64b807-da57-4bc4-8482-a38030bc0b9e>
2012-03-10 11:52:09 +0000

    Porting Uart code and reducing the scope of the driver to simply get and receive single characters

---
+++ BLUEsat-CSC/Drivers/uart/include/uart.h
@@ -0,0 +1,46 @@
+/**
+ *  \file uart.h
+ *
+ *  \brief UART Driver to allow the writing and reading
+ *         of characters out the UART port
+ *
+ *  \author $Author: Colin Tan $
+ *  \version 1.0
+ *
+ *  $Date: 2010-05-15 17:16:42 +1000 (Sat, 15 May 2010) $
+ *  \warning No Warnings for now
+ *  \bug No Bugs for now
+ *  \note No Notes for now
+ */
+ 
+#ifndef UART_H_
+#define UART_H_
+
+/**
+ * \brief Initialise the pins to UART port to allow serial
+ *        communications.
+ */
+unsigned portLONG Comms_UART_Init(void);
+
+/**
+ * \brief Reads a single character off the port
+ *
+ * \param[in] xBlockTime Time to block, in ticks,  while waiting for the character.
+ * \param[in] pcRxedChar Pointer to receive buffer for a single character.
+ *
+ * \returns pdTrue for success or pdFalse for failure
+ */
+signed portBASE_TYPE Comms_UART_Read_Char( signed portCHAR *pcRxedChar, portTickType xBlockTime );
+
+/**
+ * \brief Writes a single character onto the port
+ *
+ * \param[in] xBlockTime Time to block, in ticks,  while waiting for the character.
+ * \param[out] cOutChar Output character to be written.
+ *
+ * \returns pdTrue for success or pdFalse for failure
+ */
+signed portBASE_TYPE Comms_UART_Write_Char( signed portCHAR cOutChar, portTickType xBlockTime );
+
+
+#endif /* UART_H_ */
