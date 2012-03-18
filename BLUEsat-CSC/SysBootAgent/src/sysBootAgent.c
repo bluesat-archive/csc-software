24b8d3b6f429263475a69747b2db9f912cf58129
jamesq <jamesq@da64b807-da57-4bc4-8482-a38030bc0b9e>
2012-03-18 09:16:56 +0000

    Commented sysBootAgent.c

--- BLUEsat-CSC/SysBootAgent/src/sysBootAgent.c
+++ BLUEsat-CSC/SysBootAgent/src/sysBootAgent.c
@@ -11,46 +11,56 @@
 
 unsigned int initDrivers(void)
 {
-	#ifdef UART_H_
-		Comms_UART_Init();
-	#endif
+#ifdef UART_H_
+	//UART driver
+	Comms_UART_Init();
+#endif
 
-	#ifdef EMC_H_
-		EMC_Init();
-	#endif
+#ifdef EMC_H_
+	//External Memory Controller driver
+	EMC_Init();
+#endif
+
+#ifdef INTERNAL_FLASH_H_
+	//Internal Flash Memory driver
+	/* NO INITIALISATION REQUIRED */
+#endif
 
 	return 0;
 }
 
-#define COMMAND_TASK_PRIORITY	30
-#define DEBUG_TASK_PRIORITY		25
+//Priority used for all service tasks
+#define SERV_TASK_PRIORITY	30
 
 unsigned int initServices(void)
 {
-	#ifdef COMMAND_H_
-		vCommand_Init(COMMAND_TASK_PRIORITY);
-	#endif
+#ifdef COMMAND_H_
+	//Command task
+	vCommand_Init(SERV_TASK_PRIORITY);
+#endif
 
-	#ifdef DEBUG_H_
-		vDebug_Init(DEBUG_TASK_PRIORITY);
-	#endif
+#ifdef DEBUG_H_
+	//debug task
+	vDebug_Init(SERV_TASK_PRIORITY);
+#endif
 
 	return 0;
 }
 
-
-#define DEMOAPP_1_TASK_PRIORITY		20
-#define DEMOAPP_2_TASK_PRIORITY		20
+//Priority used for all application tasks
+#define APP_TASK_PRIORITY		25
 
 unsigned int initApplications(void)
 {
-	#ifdef DEMO_APPLICATION_1_H_
-		vDemoApp1_Init(DEMOAPP_1_TASK_PRIORITY);
-	#endif
+#ifdef DEMO_APPLICATION_1_H_
+	//Demonstration application 1
+	vDemoApp1_Init(APP_TASK_PRIORITY);
+#endif
 
-	#ifdef DEMO_APPLICATION_2_H_
-		vDemoApp2_Init(DEMOAPP_2_TASK_PRIORITY);
-	#endif
+#ifdef DEMO_APPLICATION_2_H_
+	//Demonstration application 2
+	vDemoApp2_Init(APP_TASK_PRIORITY);
+#endif
 
 	return 0;
 }
