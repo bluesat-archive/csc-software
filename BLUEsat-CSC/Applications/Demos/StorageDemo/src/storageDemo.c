2e1c6c5865d65639b4f6d4fb2988b77933059da6
jamesq <jamesq@da64b807-da57-4bc4-8482-a38030bc0b9e>
2012-06-30 14:02:26 +0000

    Test feature now uses append instead of store

--- BLUEsat-CSC/Applications/Demos/StorageDemo/src/storageDemo.c
+++ BLUEsat-CSC/Applications/Demos/StorageDemo/src/storageDemo.c
@@ -15,6 +15,7 @@
 #include "application.h"
 #include "storageDemo.h"
 #include "storage.h"
+#include "memory.h"
 #include "debug.h"
 #include "lib_string.h"
 
@@ -39,6 +40,8 @@ void vStorageDemo_Init(unsigned portBASE_TYPE uxPriority)
 	vActivateQueue(StorageDemo_TaskToken, STORAGE_DEMO_Q_SIZE);
 }
 
+#define TEST_BUF_SIZE	2048
+#define TEST_VALUE		0x42657374
 static portTASK_FUNCTION(vStorageDemoTask, pvParameters)
 {
 	(void) pvParameters;
@@ -46,6 +49,8 @@ static portTASK_FUNCTION(vStorageDemoTask, pvParameters)
 	portCHAR			pcInputBuf[MAX_READ_BUF_SIZE+1];
 	unsigned portSHORT	usReadLen;
 	unsigned portLONG 	ulRetSize;
+	void *pvTestBufferW = NULL;
+	void *pvTestBufferR = NULL;
 
 	vPrintMenu();
 
@@ -103,12 +108,85 @@ static portTASK_FUNCTION(vStorageDemoTask, pvParameters)
 		}
 		else if ((pcInputBuf[0] == 'F' && pcInputBuf[1] == 'F') && usReadLen == 2)
 		{
+			enResult = enMgmtSysCmd(StorageDemo_TaskToken, 5);
+		}
+		else if ((pcInputBuf[0] == 'T' && pcInputBuf[1] == 'S') && usReadLen == 4)
+		{
+			if (pvTestBufferW == NULL) pvTestBufferW = pvJMalloc(TEST_BUF_SIZE);
+
 			vDebugPrint(StorageDemo_TaskToken,
-						"FF not implemented!\n\r",
-						NO_INSERT,
+						"pvTestBufferW = %p\n\r",
+						(unsigned portLONG)pvTestBufferW,
 						NO_INSERT,
 						NO_INSERT);
-			continue;
+
+			if (pvTestBufferW != NULL)
+			{
+				for (usReadLen = 0; usReadLen < TEST_BUF_SIZE / sizeof(portLONG); ++usReadLen)
+				{
+					((unsigned portLONG *)pvTestBufferW)[usReadLen] = TEST_VALUE;
+				}
+				enResult = enDataStore(StorageDemo_TaskToken,
+										ulDeciStringToVal(&pcInputBuf[2], 2),
+										TEST_BUF_SIZE / 4,
+										(portCHAR *)pvTestBufferW);
+				enResult = enDataAppend(StorageDemo_TaskToken,
+										ulDeciStringToVal(&pcInputBuf[2], 2),
+										TEST_BUF_SIZE / 4,
+										(portCHAR *)pvTestBufferW);
+				enResult = enDataAppend(StorageDemo_TaskToken,
+										ulDeciStringToVal(&pcInputBuf[2], 2),
+										TEST_BUF_SIZE / 4,
+										(portCHAR *)pvTestBufferW);
+				enResult = enDataAppend(StorageDemo_TaskToken,
+										ulDeciStringToVal(&pcInputBuf[2], 2),
+										TEST_BUF_SIZE / 4,
+										(portCHAR *)pvTestBufferW);
+			}
+			enResult = 1;
+		}
+		else if ((pcInputBuf[0] == 'T' && pcInputBuf[1] == 'T') && usReadLen == 8)
+		{
+			if (pvTestBufferR == NULL) pvTestBufferR = pvJMalloc(TEST_BUF_SIZE);
+
+			vDebugPrint(StorageDemo_TaskToken,
+						"pvTestBufferR = %p\n\r",
+						(unsigned portLONG)pvTestBufferW,
+						NO_INSERT,
+						NO_INSERT);
+
+			if (pvTestBufferR != NULL)
+			{
+				enResult = enDataRead(StorageDemo_TaskToken,
+										ulDeciStringToVal(&pcInputBuf[2], 2),
+										ulDeciStringToVal(&pcInputBuf[4], 4),
+										TEST_BUF_SIZE - ulDeciStringToVal(&pcInputBuf[4], 4),
+										(portCHAR *)pvTestBufferR,
+										&ulRetSize);
+				vDebugPrint(StorageDemo_TaskToken,
+							"Test Read: %d bytes\n\r",
+							ulRetSize,
+							NO_INSERT,
+							NO_INSERT);
+				for (usReadLen = 0; usReadLen < TEST_BUF_SIZE - ulDeciStringToVal(&pcInputBuf[4], 4); ++usReadLen)
+				{
+					if (((unsigned portCHAR *)pvTestBufferR)[usReadLen]
+					    != ((unsigned portCHAR *)pvTestBufferW)[usReadLen + ulDeciStringToVal(&pcInputBuf[4], 4)])
+					{
+					vDebugPrint(StorageDemo_TaskToken,
+								"Invalid data: Position %d - CV:%p IV:%p\n\r",
+								usReadLen,
+								((unsigned portCHAR *)pvTestBufferW)[usReadLen + ulDeciStringToVal(&pcInputBuf[4], 4)],
+								((unsigned portCHAR *)pvTestBufferR)[usReadLen]);
+					}
+				}
+				vDebugPrint(StorageDemo_TaskToken,
+							"Compare complete!\n\r",
+							NO_INSERT,
+							NO_INSERT,
+							NO_INSERT);
+			}
+			enResult = 1;
 		}
 		else if ((pcInputBuf[0] == 'P' && pcInputBuf[1] == 'T') && usReadLen == 2)
 		{
@@ -149,12 +227,14 @@ static portTASK_FUNCTION(vStorageDemoTask, pvParameters)
 #define MENU_L03 RD@@$$$$%%%%\t- Read Data @@(DID), $$$$(Size), %%%%(offset)\n\r
 #define MENU_L04 CS@@\t\t- Check Size @@(DID)\n\r
 #define MENU_L05 DD@@\t\t- Delete slot @@(DID)\n\r
-#define MENU_L06 FF\t\t- Format\n\r
-#define MENU_L07 PT\t\t- Print FMT Table\n\r
-#define MENU_L08 MU\t\t- Display this menu\n\r
-#define MENU_L09 Note: All value in decimal\n\r
-#define MENU_L10 ----------------------------------------------------------------------------\n\r"
-#define MENU MENU_L00 MENU_L01 MENU_L02 MENU_L03 MENU_L04 MENU_L05 MENU_L06 MENU_L07 MENU_L08 MENU_L09 MENU_L10
+#define MENU_L06 TS@@\t\t- Test Set @@(DID)\n\r
+#define MENU_L07 TT@@%%%%\t- Test Test @@(DID), %%%%(offset)\n\r
+#define MENU_L08 FF\t\t- Format\n\r
+#define MENU_L09 PT\t\t- Print FMT Table\n\r
+#define MENU_L10 MU\t\t- Display this menu\n\r
+#define MENU_L11 Note: All value in decimal\n\r
+#define MENU_L12 ----------------------------------------------------------------------------\n\r"
+#define MENU MENU_L00 MENU_L01 MENU_L02 MENU_L03 MENU_L04 MENU_L05 MENU_L06 MENU_L07 MENU_L08 MENU_L09 MENU_L10 MENU_L11 MENU_L12
 
 void vPrintMenu(void)
 {
